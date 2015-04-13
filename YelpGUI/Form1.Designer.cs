6c7f14a20bd76e89661f67a485790c05194e681c
Fweston <forrest.weston@eecs.wsu.edu>
2015-04-13 08:05:33 -0700

    added all UI elements

--- YelpGUI/Form1.Designer.cs
+++ YelpGUI/Form1.Designer.cs
@@ -28,45 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.cList = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.AttributeList = new System.Windows.Forms.CheckedListBox();
+            this.SubCategoryList = new System.Windows.Forms.CheckedListBox();
+            this.MainCategoryList = new System.Windows.Forms.CheckedListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // tableLayoutPanel1
             // 
-            this.button1.Location = new System.Drawing.Point(1309, 707);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "MainCategories";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.3125F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 54.6875F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 224F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 806F));
+            this.tableLayoutPanel1.Controls.Add(this.AttributeList, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.SubCategoryList, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.MainCategoryList, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 3, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 43);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1414, 658);
+            this.tableLayoutPanel1.TabIndex = 1;
             // 
-            // cList
+            // AttributeList
             // 
-            this.cList.FormattingEnabled = true;
-            this.cList.Location = new System.Drawing.Point(13, 13);
-            this.cList.Name = "cList";
-            this.cList.Size = new System.Drawing.Size(409, 732);
-            this.cList.TabIndex = 1;
+            this.AttributeList.FormattingEnabled = true;
+            this.AttributeList.Location = new System.Drawing.Point(387, 3);
+            this.AttributeList.Name = "AttributeList";
+            this.AttributeList.Size = new System.Drawing.Size(212, 649);
+            this.AttributeList.TabIndex = 7;
+            // 
+            // SubCategoryList
+            // 
+            this.SubCategoryList.FormattingEnabled = true;
+            this.SubCategoryList.Location = new System.Drawing.Point(177, 3);
+            this.SubCategoryList.Name = "SubCategoryList";
+            this.SubCategoryList.Size = new System.Drawing.Size(204, 649);
+            this.SubCategoryList.TabIndex = 6;
+            // 
+            // MainCategoryList
+            // 
+            this.MainCategoryList.FormattingEnabled = true;
+            this.MainCategoryList.Location = new System.Drawing.Point(3, 3);
+            this.MainCategoryList.Name = "MainCategoryList";
+            this.MainCategoryList.Size = new System.Drawing.Size(168, 649);
+            this.MainCategoryList.TabIndex = 5;
+            this.MainCategoryList.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.MainCategoryList_ItemCheck);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(611, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 652);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 728);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(168, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(189, 728);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(98, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(295, 728);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(98, 21);
+            this.comboBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 702);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Day of Week";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 702);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "From";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(292, 702);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "To";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(399, 728);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(212, 21);
+            this.comboBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(396, 707);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Search For";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(623, 706);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1438, 761);
-            this.Controls.Add(this.cList);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox cList;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckedListBox AttributeList;
+        private System.Windows.Forms.CheckedListBox SubCategoryList;
+        private System.Windows.Forms.CheckedListBox MainCategoryList;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- YelpGUI/Form1.cs
+++ YelpGUI/Form1.cs
@@ -13,22 +13,53 @@ namespace YelpGUI
     public partial class Form1 : Form
     {
         MySqlConnector _mydb;
+        List<String> MainCategoryCheckedItems = new List<String>();
+        List<String> SubCategoryCheckedItems = new List<String>();
+        
         public Form1()
         {
             InitializeComponent();
             _mydb = new MySqlConnector();
-        }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string qStr = "SELECT DISTINCT cat_name FROM category ORDER BY cat_name";
+            string qStr = "SELECT DISTINCT cat_name FROM category WHERE is_main = True ORDER BY cat_name";
             List<String> qResult = _mydb.SQLSELECTExec(qStr, "cat_name");
 
             //copy query to Listbox cList
             for (int i = 0; i < qResult.Count(); i++)
             {
-                cList.Items.Add(qResult[i]);
+                MainCategoryList.Items.Add(qResult[i]);
             }
         }
+
+        private void MainCategoryList_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if(e.NewValue == CheckState.Checked)
+            {
+                var checkedItem = MainCategoryList.SelectedItem;
+                MainCategoryCheckedItems.Add(checkedItem.ToString());
+            }
+            else if (e.NewValue == CheckState.Unchecked)
+            {
+                var checkedItem = MainCategoryList.SelectedItem;
+                MainCategoryCheckedItems.Remove(checkedItem.ToString());
+            }
+            SubCategoryList_Update(sender, e);
+        }
+
+        private void SubCategoryList_Update(object sender, ItemCheckEventArgs e)
+        {
+            SubCategoryList.Items.Clear();
+            string qStr = "SELECT DISTINCT cat_name FROM category WHERE is_main = False ORDER BY cat_name";
+            List<String> qResult = _mydb.SQLSELECTExec(qStr, "cat_name");
+            //copy query to Listbox cList
+            for (int i = 0; i < qResult.Count(); i++)
+            {
+                SubCategoryList.Items.Add(qResult[i]);
+            }
+        }
+
+
+
+    
     }
 }
